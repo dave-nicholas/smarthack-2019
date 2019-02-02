72b7739f890110412ce24fbe97eb027d36c30122
Dave Nicholas <davenicholasuk@gmail.com>
2019-02-02 09:27:56 +0000

    Add logging

--- src/test.js
+++ src/test.js
@@ -1,11 +1,13 @@
 const Gpio = require("onoff").Gpio;
-const ledIO = new Gpio(17, "out");
+const ledIO = new Gpio(2, "out");
 const monkeyIO = new Gpio(4, "out");
 
 ledIO.writeSync(1);
 monkeyIO.writeSync(1);
+console.log(' :on');
 
 setTimeout(() => {
   ledIO.writeSync(0);
   monkeyIO.writeSync(0);
+  console.log(' :off')
 }, 3000);
