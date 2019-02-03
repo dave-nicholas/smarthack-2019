39ec47c3de29b6258a821be0447942c3e25eb1e8
Dave Nicholas <davenicholasuk@gmail.com>
2019-02-03 18:54:07 +0000

    refactor and add sound

---
+++ src/gpio.ts
@@ -0,0 +1,29 @@
+import { Gpio } from "onoff";
+
+const monkeyGPIO = new Gpio(4, "out");
+const leftEyeGPIO = new Gpio(2, "out");
+const rightEyeGPIO = new Gpio(17, "out");
+
+export const monkeyDance = () => {
+  monkeyGPIO.writeSync(1);
+  setTimeout(() => monkeyGPIO.writeSync(0), 2000);
+};
+
+const flashingEyes = () => {
+  if (leftEyeGPIO.readSync() === 0) {
+    leftEyeGPIO.writeSync(1);
+    rightEyeGPIO.writeSync(1);
+  } else {
+    leftEyeGPIO.writeSync(0);
+    rightEyeGPIO.writeSync(0);
+  }
+};
+
+export const flashEyes = (time: number) => {
+  const interval = setInterval(flashingEyes, 250);
+  setTimeout(() => {
+    clearInterval(interval);
+    leftEyeGPIO.writeSync(0);
+    rightEyeGPIO.writeSync(0);
+  }, time);
+};
