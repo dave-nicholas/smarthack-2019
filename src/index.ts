b2fd15969bf839d9c2bd54a1c89973f1f79201ee
Dave Nicholas <davenicholasuk@gmail.com>
2019-02-04 08:34:07 +0000

    Add refactory and audio

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,11 @@
 import * as http from "http";
-import createHandler from "github-webhook-handler";
-import "dotenv/config";
-
-import { monkeyDance, flashEyes } from "./gpio";
-import { mushMuscle } from "./sound";
-
-console.log("gh secret:", process.env.GIT_WEBHOOK_SECRET);
-
-const handler = createHandler({
-  path: "/webhook",
-  secret: process.env.GIT_WEBHOOK_SECRET || ""
-});
+import { gitWebHookHandler } from "./gitwebhooks";
 
 http
   .createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    handler(req, res, (err: Error) => {
+    gitWebHookHandler(req, res, (err: Error) => {
       res.statusCode = 404;
       res.end("no such location");
     });
   })
   .listen(4567);
-
-handler.on("error", function(err) {
-  console.error("Error:", err.message);
-});
-
-handler.on("push", function(event) {
-  monkeyDance();
-  flashEyes(10000);
-  console.log(
-    "Received a push event for %s to %s",
-    event.payload.repository.name,
-    event.payload.ref
-  );
-});
-
-handler.on("issues", function(event) {
-  monkeyDance();
-  mushMuscle.play();
-  flashEyes(25000);
-  console.log(
-    "Received an issue event for %s action=%s: #%d %s",
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title
-  );
-});
