6acd965d948b1fc6de2cd179604ab64da45eed07
Dave Nicholas <davenicholasuk@gmail.com>
2019-02-04 08:34:28 +0000

    add githook file

---
+++ src/gitwebhooks.ts
@@ -0,0 +1,39 @@
+import createHandler from "github-webhook-handler";
+import "dotenv/config";
+
+import { monkeyDance, flashEyes } from "./gpio";
+import { mushMuscle, vote, questions } from "./sound";
+
+export const gitWebHookHandler = createHandler({
+  path: "/webhook",
+  secret: process.env.GIT_WEBHOOK_SECRET || ""
+});
+
+gitWebHookHandler.on("error", err => console.error("Error:", err.message));
+
+const playVote = () => {
+  flashEyes(5000);
+  vote.play();
+};
+
+gitWebHookHandler.on("pull_request_review", event => playVote());
+gitWebHookHandler.on("pull_request_review_comment", event => playVote());
+
+gitWebHookHandler.on("push", async event => {
+  await mushMuscle.play();
+  monkeyDance();
+  flashEyes(10000);
+});
+
+gitWebHookHandler.on("issues", async event => {
+  await questions.play();
+  monkeyDance();
+  flashEyes(25000);
+  console.log(
+    "Received an issue event for %s action=%s: #%d %s",
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title
+  );
+});
