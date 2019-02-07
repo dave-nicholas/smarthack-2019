c598fd18ea21d5ba259c483291f6e865326e4cd9
Martin Carder <martincarder@hotmail.co.uk>
2019-02-07 15:56:54 +0000

    Slack helpers

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { gitWebHookHandler } from "./gitwebhooks";
 import { speak }  from "./speak";
 import { helloCutiePie } from "./sound";
 import { flashEyes } from './gpio';
+import path from "path";
+import { verify, postImg } from "./slack";
 
 const app = express();
 
@@ -11,14 +13,9 @@ const port = 4567;
 
 app.use(bodyParser.json());
 
-app.post('/slack', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  console.log(req.body)
-  res.send(JSON.stringify({"challenge":req.body.challenge}));
-});
+app.post('/slack', verify);
 
 app.post('/', (req, res) => {
-  console.log("!!!!!")
   gitWebHookHandler(req, res, (err: Error) => {
     res.statusCode = 404;
     res.end("no such location");
@@ -26,4 +23,13 @@ app.post('/', (req, res) => {
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const loadImg = () => {
+  
+  const filePath = path.resolve('src', 'test.png');
+
+  postImg(filePath, "And another image");
+}
+
+// loadImg();
 
