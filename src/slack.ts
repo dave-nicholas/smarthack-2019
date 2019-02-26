f7c668e28ff2e91cbb6b9c3d23c6db8dfdd9b4f5
Dave Nicholas <davenicholasuk@gmail.com>
2019-02-26 08:40:32 +0000

    Changes to stop monkey from being destoryed

--- src/slack.ts
+++ src/slack.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import fs from "fs";
-import { WebClient } from '@slack/client';
-import { flashEyes } from './gpio';
-import { speak }  from "./speak";
+import { WebClient } from "@slack/client";
+import { flashEyes } from "./gpio";
+import { speak } from "./speak";
 import { Raspistill } from "node-raspistill";
 import { monkeyDance } from "./gpio";
 
@@ -13,44 +13,56 @@ const channels = process.env.SLACK_CHANNELS;
 
 const web = new WebClient(token);
 
-export const verify = (req: express.Request, res: express.Response) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({"challenge":req.body.challenge}));
+let lastDance = 0;
 
-  if(req.body.event.type = "app_mention") {
-    console.log(req.body.event.text.replace(/<.*>/, ''));
+export const verify = (req: express.Request, res: express.Response) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ challenge: req.body.challenge }));
+
+  if ((req.body.event.type = "app_mention")) {
+    console.log(req.body.event.text.replace(/<.*>/, ""));
     flashEyes(5000);
-    speak(req.body.event.text.replace(/<.*>/, ''));
+    speak(req.body.event.text.substring(0, 140).replace(/<.*>/, ""));
   }
-}
+};
 
 export const photo = (req: express.Request, res: express.Response) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send()
-  sendCameraToSlack("Slack photo")
-}
+  res.setHeader("Content-Type", "application/json");
+  res.send();
+  sendCameraToSlack("Slack photo");
+};
 
 export const dance = (req: express.Request, res: express.Response) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send()
-  monkeyDance();
-  flashEyes(25000, true);
-}
+  if (Date.now() - lastDance > 5400000) {
+    // 90 minutes
+    lastDance = Date.now();
+    res.setHeader("Content-Type", "application/json");
+    res.send();
+    monkeyDance();
+    flashEyes(25000, true);
+  } else {
+    web.chat.postMessage({
+      channel: channels || "",
+      text:
+        "I am too tired to dance right now. What am I? A performing Monkey!!!"
+    });
+  }
+};
 
 export const sendCameraToSlack = (message: string) => {
-  camera.takePhoto('photo').then((photo) => {
-    postImg(photo, message)
-  })
-}
+  camera.takePhoto("photo").then(photo => {
+    postImg(photo, message);
+  });
+};
 
 export const postImg = (file: any, title: string) => {
-
-  web.files.upload({ file, channels, title })
-    .then((response) => {
+  web.files
+    .upload({ file, channels, title })
+    .then(response => {
       console.log(`File uploaded as Stream`);
     })
     .catch((error: any) => {
-      console.log('File upload as Stream error:');
+      console.log("File upload as Stream error:");
       console.log(error);
     });
-}
+};
